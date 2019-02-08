1d1476df89b9e7029a2da0dec5a4ecd996d0ae9f
Konstantin Lampalzer <konstantin@localhost.systems>
2019-02-08 12:22:11 +0100

    exit dwh1

--- tasks/dwh_1.sql
+++ tasks/dwh_1.sql
@@ -1,3 +1,4 @@
+-- 2)
 drop tablespace dwh including contents and datafiles cascade constraints;
 
 create tablespace dwh
@@ -5,58 +6,104 @@ create tablespace dwh
        size 10m reuse 
        autoextend on maxsize 30m;
 
-drop table Verkaufszahl;
-drop table Produkt;
-drop table Ort;
-drop table Zeit;
+-- 2)
+drop table verkaufszahl;
+drop table produkt;
+drop table ort;
+drop table zeit;
 
-create table Produkt (
-    produkt         varchar(64) primary key,
-    marke           varchar(64),
-    hersteller      varchar(64),
-    produktgruppe   varchar(54)
+alter session
+      set NLS_DATE_FORMAT = 'dd.mm.yyyy';
+-- SET datestyle = "ISO, DMY";
+
+create table ort (
+    filiale varchar(32) primary key,
+    stadt varchar(32) not null,
+    region varchar(16) not null,
+    land varchar(3) not null
 );
 
-create table Ort (
-    filiale varchar(64) primary key,
-    stadt   varchar(64),
-    region  varchar(64),
-    land    varchar(64)
+create table zeit (
+    datum date primary key,
+    woche int not null,
+    monat int not null,
+    quartal int not null,
+    jahr int not null
 );
 
-create table Zeit (
-    datum   date primary key,
-    woche   int,
-    monat   int,
-    quartal int,
-    jahr    int
+create table produkt (
+    produkt varchar(32) primary key,
+    marke varchar(32) not null,
+    hersteller varchar(32) not null,
+    produktgruppe varchar(32) not null
 );
 
-create table Verkaufszahl (
-    filiale references Ort,
-    produkt references Produkt,
-    datum references Zeit,
-    anzahl int    
+create table verkaufszahl (
+    filiale varchar(32) not null references ort,
+    produkt varchar(32) not null references produkt,
+    datum date not null references zeit,
+    anzahl int not null
 );
 
-insert into Ort values ('Hamburg', 'Hamburg', 'Nord', 'D');
-insert into Ort values ('Leipzig', 'Leipzig', 'Ost', 'D');
-insert into Ort values ('Stuttgart', 'Stuttgart', 'Süd', 'D');
-insert into Ort values ('Bremen-Nord', 'Bremen', 'Nord', 'D');
-insert into Ort values ('Bremen-Süd', 'Bremen', 'Nord', 'D');
-insert into Ort values ('München', 'München', 'Süd', 'D');
+-- 3)
+insert into ort values('Hamburg', 'Hamburg', 'Nord', 'D');
+insert into ort values('Leipzig', 'Leipzig', 'Ost', 'D');
+insert into ort values('Stuttgart', 'Stuttgart', 'Süd', 'D');
+insert into ort values('Bremen-Nord', 'Bremen', 'Nord', 'D');
+insert into ort values('Bremen-Süd', 'Bremen', 'Nord', 'D');
+insert into ort values('München', 'München', 'Süd', 'D');
 
-insert into Produkt values ('Pizza Funghi', 'Gourmet-Pizza', 'Frost GmbH', 'Tiefkühlkost');
-insert into Produkt values ('Pizza Hawaii', 'Gourmet-Pizza', 'Frost GmbH', 'Tiefkühlkost');
-insert into Produkt values ('Pizza Napoli', 'Pizza', 'TK-Pizza AG', 'Tiefkühlkost');
-insert into Produkt values ('Pizza Vegetale', 'Good&Cheap', 'Frost GmbH', 'Tiefkühlkost');
-insert into Produkt values ('Pizza Calzione', 'Pizza', 'TK-Pizza AG', 'Tiefkühlkost');
+insert into zeit values('5.1.2006', 1, 1, 1, 2006);
+insert into zeit values('12.1.2006', 2, 1, 1, 2006);
+insert into zeit values('13.2.2006', 7, 2, 1, 2006);
+insert into zeit values('23.2.2006', 8, 2, 1, 2006);
+insert into zeit values('4.3.2006', 9, 3, 2, 2006);
+insert into zeit values('7.4.2006', 14, 4, 2, 2006);
+insert into zeit values('25.4.2006', 17, 4, 2, 2006);
 
-insert into Verkaufszahl ('Hamburg', 'Pizza Funghi', '', 78);
-insert into Verkaufszahl ('Hamburg', 'Pizza Funghi', '', 67);
-insert into Verkaufszahl ('Leipzig', 'Pizza Hawaii', '', 42);
-insert into Verkaufszahl ('München', 'Pizza Calzione', '', 53);
-insert into Verkaufszahl ('Stuttgart', 'Pizza Napoli', '', 23);
-insert into Verkaufszahl ('Bremen-Nord', 'Pizza Funghi', '', 69);
-insert into Verkaufszahl ('Bremen-Süd', 'Pizza Vegetale', '', 45);
-insert into Verkaufszahl ('Stuttgart', 'Pizza Hawaii', '', 92);
+insert into produkt values('Pizza Funghi', 'Gourmet-Pizza', 'Frost GmbH', 'Tiefkühlkost');
+insert into produkt values('Pizza Hawaii', 'Gourmet-Pizza', 'Frost GmbH', 'Tiefkühlkost');
+insert into produkt values('Pizza Napoli', 'Pizza', 'TK-Pizza AG', 'Tiefkühlkost');
+insert into produkt values('Pizza Vegetale', 'Good&Cheap', 'Frost GmbH', 'Tiefkühlkost');
+insert into produkt values('Pizza Calzione', 'Pizza', 'TK-Pizza AG', 'Tiefkühlkost');
+
+insert into verkaufszahl values('Hamburg', 'Pizza Funghi', '5.1.2006', 78);
+insert into verkaufszahl values('Hamburg', 'Pizza Funghi', '12.1.2006', 67);
+insert into verkaufszahl values('Leipzig', 'Pizza Hawaii', '12.1.2006', 42);
+insert into verkaufszahl values('München', 'Pizza Calzione', '13.2.2006', 53);
+insert into verkaufszahl values('Stuttgart', 'Pizza Napoli', '23.2.2006', 23);
+insert into verkaufszahl values('Bremen-Nord', 'Pizza Funghi', '4.3.2006', 69);
+insert into verkaufszahl values('Bremen-Süd', 'Pizza Vegetale', '7.4.2006', 45);
+insert into verkaufszahl values('Stuttgart', 'Pizza Hawaii', '25.4.2006', 92);
+
+-- 4)
+select o.region, z.jahr, p.hersteller
+  from verkaufszahl v join ort o on v.filiale=o.filiale
+  join zeit z on v.datum=z.datum
+  join produkt p on v.produkt=p.produkt
+ where o.region='Nord' and z.jahr='2006'
+ group by o.region, z.jahr, p.hersteller
+;
+
+select o.region, p.marke, z.jahr, sum(v.anzahl) as anzahl
+  from verkaufszahl v join ort o on v.filiale=o.filiale
+  join zeit z on v.datum=z.datum
+  join produkt p on v.produkt=p.produkt
+ group by o.region, p.marke, z.jahr
+;
+
+-- 5)
+drop materialized view region_marke_jahr;
+
+create materialized view region_marke_jahr as 
+    select o.region, p.marke, z.jahr, sum(v.anzahl) as anzahl
+      from verkaufszahl v join ort o on v.filiale=o.filiale
+      join zeit z on v.datum=z.datum
+      join produkt p on v.produkt=p.produkt
+    group by o.region, p.marke, z.jahr
+;
+
+-- 6)
+select * 
+  from region_marke_jahr
+;
