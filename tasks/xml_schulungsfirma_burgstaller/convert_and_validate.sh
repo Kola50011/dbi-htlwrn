bb7a7e2b380756be916f5efff34270226cb09398
Konstantin Lampalzer <konstantin@localhost.systems>
2019-03-18 14:40:04 +0100

    RNC for kurslisten

--- tasks/xml_schulungsfirma_burgstaller/convert_and_validate.sh
+++ tasks/xml_schulungsfirma_burgstaller/convert_and_validate.sh
@@ -11,5 +11,13 @@ java -jar ../../tools/jing/bin/jing.jar -t -c schulungsfirma.rnc schulungsfirma.
 xsltproc -o kurslisten.xml schulungsfirma.xslt schulungsfirma.xml
 xsltproc -o kurslisten.html kurslisten.xslt kurslisten.xml
 
+java -jar  ../../tools/trang/trang.jar -I xml -O rnc kurslisten.xml kurslisten.rnc
+cat kurslisten.rnc | sed -i "s/NCName/string/"  kurslisten.rnc
+cat kurslisten.rnc | sed -i "s/NMTOKEN/date/"  kurslisten.rnc
+
+java -jar  ../../tools/trang/trang.jar -I rnc -O dtd kurslisten.rnc kurslisten.dtd
+java -jar ../../tools/trang/trang.jar -I rnc -O rng kurslisten.rnc kurslisten.rng
+java -jar  ../../tools/trang/trang.jar -I rnc -O xsd kurslisten.rnc kurslisten.xsd
+
 sed -i '2i <?xml-stylesheet type="text/xsl" href="kurslisten.xslt"?>' kurslisten.xml
 google-chrome --allow-file-access-from-files kurslisten.xml
--- tasks/xml_schulungsfirma_burgstaller/schulungsfirma.xslt
+++ tasks/xml_schulungsfirma_burgstaller/schulungsfirma.xslt
@@ -1,6 +1,6 @@
 <?xml version="1.0"?>
 <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
-    <xsl:output method="xml" />
+    <xsl:output method="xml" indent="yes"/>
     <xsl:template match="/">
         <kurslisten>
             <xsl:apply-templates select="schulungsfirma/kurs" />
