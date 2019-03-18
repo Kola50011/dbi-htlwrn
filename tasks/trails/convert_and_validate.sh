fa309083d2efbba5ffd53946fda8feb038f38819
Konstantin Lampalzer <konstantin@localhost.systems>
2019-03-18 08:42:06 +0100

    Fix typo

--- tasks/trails/convert_and_validate.sh
+++ tasks/trails/convert_and_validate.sh
@@ -3,4 +3,4 @@ java -jar  ../../tools/trang/trang.jar -I rnc -O dtd trails.rnc trails.dtd
 java -jar ../../tools/trang/trang.jar -I rnc -O rng trails.rnc trails.rng
 java -jar  ../../tools/trang/trang.jar -I rnc -O xsd trails.rnc trails.xsd
 
-java -jar ../../tools/jing/bin/jing.jar -t -c schulungsfirtrailsma.rnc trails.xml
+java -jar ../../tools/jing/bin/jing.jar -t -c trails.rnc trails.xml
